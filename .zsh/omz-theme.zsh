fcd583be6ab13763db17e7f8f5a4c7c9e2a2a058
Maxime Chambre <enzu.mc@gmail.com>
2026-05-29 03:26:03 +0200

    feat: add base zshrc with basic omz scripts

---
+++ .zsh/omz-theme.zsh
@@ -0,0 +1,19 @@
+ZSH_THEME=
+fpath+=($HOME/.zsh/pure)
+
+autoload -U promptinit; promptinit
+
+zstyle :prompt:pure:user show yes
+zstyle :prompt:pure:host show yes
+zstyle :prompt:pure:git:action show yes
+zstyle :prompt:pure:exection_time show yes
+zstyle :prompt:pure:git:stash show no
+
+zstyle :prompt:pure:user color '#a7a7a7'
+zstyle :prompt:pure:host color '#a7a7a7'
+zstyle :prompt:pure:path color '#4dbbff'
+zstyle :prompt:pure:git:branch color '#ff4d2a'
+
+zstyle :prompt:pure:virtualenv color '#ffffff'
+
+prompt pure
---
+++ .zshrc
@@ -0,0 +1,13 @@
+# Config dir paths
+export ZSH="$HOME/.zsh"
+export OMZ="$HOME/.oh-my-zsh"
+
+# OMZ config
+. "$ZSH/omz-theme.zsh"
+. "$ZSH/omz-plugins.zsh"
+
+# OMZ sourcing
+. "$OMZ/oh-my-zsh.sh"
+
+# Other config
+. "$ZSH/alias.zsh"
